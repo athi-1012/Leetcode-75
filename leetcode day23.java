fee2d6f7f8c1ecf097f69e61a90bc715019ff9a8
athi-1012 <111171543+athi-1012@users.noreply.github.com>
2023-12-26 21:34:36 +0530

    Add files via upload

---
+++ leetcode day23.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+        for(char ch : word1.toCharArray())
+            arr1[ch - 'a']++;
+        for(char ch : word2.toCharArray())
+            arr2[ch - 'a']++;
+        for(int i=0;i<26;i++) {
+            if (arr1[i] == arr2[i]) {
+                continue;
+            }
+            if(arr1[i] == 0 || arr2[i] == 0) {
+                return false;
+            }
+        }
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        for(int i=0;i<26;i++) {
+            if(arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+}
+}
