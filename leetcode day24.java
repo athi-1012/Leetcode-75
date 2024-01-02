6f667909017debf517f89992f4e74507dcf8f121
athi-1012 <111171543+athi-1012@users.noreply.github.com>
2024-01-02 22:41:11 +0530

    Add files via upload

---
+++ leetcode day24.java
@@ -0,0 +1,16 @@
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int start=1;
+        int end=n;
+        while(start<=end){
+        int mid=start+(end-start)/2;
+        if(guess(mid)==0)
+            return mid;
+        if(guess(mid)==1)
+            start= mid+1;
+        else
+            end=mid-1;      
+        }
+        return -1;
+    }
+}
